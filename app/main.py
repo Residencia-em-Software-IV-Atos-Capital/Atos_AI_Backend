52764ea9c4844c0618c19c20796b67011ddc36c5
pedrocarvalho-13 <pedrogui.carvalho13@gmail.com>
2025-10-09 08:42:36 -0300

    Modificando cors, transformando API em pública

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 from app.routes import data_routes
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,                      
+    allow_credentials=True,                     
+    allow_methods=["*"],                        
+    allow_headers=["*"],                        
+)
 # Inclui os endpoints do seu router
 app.include_router(data_routes.router)
 
