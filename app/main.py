ff66a93f9e3ed912e75c468d7593fdd047d97a0c
Carlos Henrique Santos Silveira <109424074+ByteCarlos@users.noreply.github.com>
2025-11-21 18:21:36 -0300

    hotfix: config cors

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import data_routes
 
 app = FastAPI()
 
-# Inclui os endpoints do seu router
+# Configuração de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],          # ou ["http://localhost:3000"] se preferir restringir
+    allow_credentials=True,
+    allow_methods=["*"],          # permite GET, POST, OPTIONS, etc.
+    allow_headers=["*"],          # permite Content-Type, Authorization, etc.
+)
+
+# Inclui o router
 app.include_router(data_routes.router)
 
 @app.get("/")
